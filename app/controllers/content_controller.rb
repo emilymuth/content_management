2eaa2a9172240c5570e4df1cd1b6a14472d10163
Malick <malick.diarra@excella.com>
2016-08-05 18:56:34 +0000

    update initial controller

---
+++ app/controllers/content_controller.rb
@@ -0,0 +1,7 @@
+class ContentController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  def grab_content(feature_name)
+
+  end
+end
