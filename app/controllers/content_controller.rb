2d1d0819aa03339bebc892d18b1a4f044416bba1
Malick <malick.diarra@excella.com>
2016-08-08 20:42:40 +0000

    updated copy

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,7 +1,13 @@
-class ContentController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+class ContentController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   def grab_content(feature_name)
-  	
+  	render json: { test_file: 'good files', status: 200}, status: 200
+  end
+
+  private
+
+  def record_not_found
+    render json: { message: '404 Not Found', status: 404 }, status: 404
   end
 end
